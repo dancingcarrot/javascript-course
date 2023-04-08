bd1f99ce650d14628bfefc8d107751d7d7ef22a0
dancingcarrot <loopy456@naver.com>
2023-04-08 20:50:51 +0900

    update

---
+++ 8/8i.js
@@ -0,0 +1,7 @@
+
+      const randomNumber = Math.random();
+      const result = randomNumber < 0.5 ? 'heads' : 'tails';
+
+      const guess = 'heads';
+      console.log(guess === result ? 'You win!' : 'You lose!');
+
